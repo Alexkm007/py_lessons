6fb689ecf83322cce6685132b91007fd380e3bec
Aleksei Koshelev <koschelev@gmail.com>
2019-12-02 19:06:28 +0300

    new commit

---
+++ environmets/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alex/eclipse-workspace/py_lessons/py_lessons/environmets/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
