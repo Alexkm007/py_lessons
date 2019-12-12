035a0fdebf7a6f7ee50595530684ee0350b05db1
Aleksei Koshelev <koschelev@gmail.com>
2019-12-12 20:35:01 +0300

    Коммит

---
+++ environmets/myvenv/mysite/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.conf.urls.i18n import urlpatterns
+
+app_name = 'blog'
+
+urlpatterns = [
+    # post views
+    path ('',views.post_list,name = 'post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,name = 'post_detail'),
+    ]
